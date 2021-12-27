eda885d637e98967b975c5bd6a189c68121e7d23
Bi Wu <alerk_star@outlook.com>
2021-12-28 02:25:38 +0800

    initialized 6-enum;

---
+++ enum-pattern/src/main.rs
@@ -0,0 +1,23 @@
+enum IpAddrKind {
+  V4(String),
+  V6(String),
+}
+
+struct IpAddr {
+  kind: IpAddrKind,
+  address: String,
+}
+
+fn main() {
+  let four = IpAddrKind::V4;
+  let six = IpAddrKind::V6;
+
+  let localhost = IpAddr {
+    kind: IpAddrKind::V4(String::from("127.0.0.1")),
+    address: String::from("127.0.0.1")
+  };
+}
+
+fn route(ip_kind: IpAddrKind) {
+
+}
