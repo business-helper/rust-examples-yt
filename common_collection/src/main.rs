34348bbeeb8072570c570fd571a635a6d2f38d4f
Bi Wu <alerk_star@outlook.com>
2022-01-07 20:50:53 +0800

    hashmap manipulation;

--- common_collection/src/main.rs
+++ common_collection/src/main.rs
@@ -1,4 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
+use std::collections::HashMap;
 
 fn main() {
   func1();
@@ -12,6 +13,12 @@ fn main() {
   func5();
 
   func6();
+
+  func7();
+
+  func8();
+
+  func9();
 }
 
 fn func1() {
@@ -115,4 +122,58 @@ fn func6() {
   for g in "العددي".graphemes(true) {
     println!("{}", g);
   }
+}
+
+// HashMap
+fn func7() {
+  println!("----- func7 -----");
+  let blue = String::from("Blue");
+  let yellow = String::from("Yellow");
+
+  let mut scores = HashMap::new();
+
+  scores.insert(blue, 10);
+  scores.insert(yellow, 50);
+
+  // blue ownership -> hashmap.
+  // println!("{}", blue);
+
+  let team_name = String::from("Blue");
+  let score = scores.get(&team_name);
+  
+  // println!("Blue team score is {:?}", score);
+
+  for (key, value) in &scores {
+    println!("{}: {}", key, value);
+  }
+}
+
+fn func8() {
+  println!("----- func8 -----");
+  let mut scores = HashMap::new();
+
+  scores.insert(String::from("Blue"), 10);
+  scores.insert(String::from("Blue"), 20);
+
+  scores.entry(String::from("Yellow")).or_insert(30);
+  scores.entry(String::from("Yellow")).or_insert(40);
+
+  for (key, value) in &scores {
+    println!("{}: {}", key, value);
+  }
+}
+
+fn func9() {
+  println!("----- func9 -----");
+
+  let text = "hello world wonderful world";
+
+  let mut map = HashMap::new();
+
+  for word in text.split_whitespace() {
+    let count = map.entry(word).or_insert(0);
+    *count += 1;
+  }
+
+  println!("{:?}", map);
 }
