a2a3ffeef0f823685954b4dabac34ea1fe3398c6
Bi Wu <alerk_star@outlook.com>
2022-01-07 11:16:45 +0800

    struct in module and public

--- module/restaurant/src/lib.rs
+++ module/restaurant/src/lib.rs
@@ -1,8 +1,63 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
+
+mod front_of_house {
+  pub mod hosting {
+    pub fn add_to_waitlist() {}
+
+    fn seat_at_table() {}
+  }
+
+  mod serving {
+    fn take_order() {}
+
+    fn server_order() {}
+
+    fn take_payment() {}
+  }
+}
+
+// mod back_of_house {
+//   fn fix_incorrect_order() {
+//     cook_order();
+//     super::serve_order();
+//   }
+
+//   fn cook_order() {}
+// }
+
+mod back_of_house {
+  pub struct Breakfast {
+    pub toast: String,
+    seasonal_fruit: String,
+  }
+
+  impl Breakfast {
+    pub fn summer(toast: &str) -> Breakfast {
+      Breakfast {
+        toast: String::from(toast),
+        seasonal_fruit: String::from("peaches"),
+      }
     }
+  }
+}
+
+pub fn eat_at_restaurant() {
+  // Absolute path
+  crate::front_of_house::hosting::add_to_waitlist();
+
+  // Relative path
+  front_of_house::hosting::add_to_waitlist();
+}
+
+pub fn eat_at_back_of_house() {
+  let mut meal = back_of_house::Breakfast::summer("Rye");
+
+  meal.toast = String::from("Wheat");
 }
