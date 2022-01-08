b8c7446f3012ba5f8b9fad65b673b5f9a57f2382
Bi Wu <alerk_star@outlook.com>
2022-01-08 11:13:33 +0800

    generic types with enum;

--- generic_types/README.md
+++ generic_types/README.md
@@ -1,2 +1,3 @@
 # [Generic Types in Rust](https://www.youtube.com/watch?v=6rcTSxPJ6Bw)
 
+for example, when comparing number or chars, to use a function to get max from the vectors of integers or chars, the function must accept the generic parameters, or we must defined multiple functions for each data types.
--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,9 +1,33 @@
 fn main() {
+    func3();
+    return;
     func2();
     func1();
-    return;
+    // return;
 }
 
+struct Point<T, U> {
+  x: T,
+  y: U,
+}
+
+enum Option<T> {
+  Some(T),
+  None,
+}
+
+enum Result<T, E> {
+  Ok(T),
+  Err(E),
+}
+
+fn func3() {
+  let p1 = Point { x: 5, y: 10 };
+  let p2 = Point { x: 5.0, y: 10.0 };
+  let p3 = Point { x: 5, y: 10.0 };
+}
+
+// apply generic type
 fn func2() {
     println!("------- func 1 --------");
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
